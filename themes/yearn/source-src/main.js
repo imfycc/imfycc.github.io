3bcf8edff48023b71adc4a24c24d85be5b58f89a
Youthink <hufy3651@foxmail.com>
2018-10-20 12:19:05 +0800

    theme:support baidu statistics

--- themes/yearn/source-src/main.js
+++ themes/yearn/source-src/main.js
@@ -41,20 +41,6 @@ function toggleHeaderNavBar() {
   });
 }
 
-//http to https
-var host = "hufangyun.com" || "www.hufangyun.com";
-if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
-
-// Baidu Analytics
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ae46047b557574a12bba544bea130043";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
 // search
 var searchData;
 var search = document.getElementById('search');
